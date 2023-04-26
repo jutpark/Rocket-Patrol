d37177c2881150bf55d1992a1bb283dfc468fd58
Jutpark <jutpark@ucsc.edu>
2023-04-25 22:18:20 -0700

    reset

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -22,10 +22,13 @@ if(this.isFiring&&this.y>=borderUISize*3+borderPadding){
     this.y-=this.moveSpeed;
 }
 if(this.y<=borderUISize*3+borderPadding){
-    this.isFiring=false;
-    this.y=game.config.height-borderUISize-borderPadding;
+    this.reset();
 }
         }
 
+    }
+    reset(){
+        this.isFiring=false;
+        this.y=game.config.height-borderUISize-borderPadding;
     }
   }
