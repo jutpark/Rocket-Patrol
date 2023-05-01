954afabd2bb73a2b3065a0e825cbb5bbfaa4cfa7
Jutpark <jutpark@ucsc.edu>
2023-05-01 16:32:51 -0700

    Newship

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -40,6 +40,7 @@ class Menu extends Phaser.Scene {
           // easy mode
           game.settings = {
             spaceshipSpeed: 3,
+            NewshipSpeed: 8,
             gameTimer: 60000    
           }
           this.sound.play('sfx_select');
@@ -49,6 +50,7 @@ class Menu extends Phaser.Scene {
           // hard mode
           game.settings = {
             spaceshipSpeed: 4,
+            NewshipSpeed: 12,
             gameTimer: 45000    
           }
           this.sound.play('sfx_select');
