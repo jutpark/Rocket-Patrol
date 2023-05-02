9da6f439e97d69ca8cdfd708f2a86ed514ebac09
Jutpark <jutpark@ucsc.edu>
2023-05-01 18:56:02 -0700

    added comments with stuff done so far

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
+/*
+Justin Park
+new ship type + 15
+move while firing +5
+add time when hit newship +15
+increase chip speed after 30 seconds +5
+added visible timer +10
+added godawful explosion sounds +10
+added rainbow explosion particles +15
+https://www.youtube.com/watch?v=LEDPCfot_GY&ab_channel=MitchellHudson
+https://labs.phaser.io/index.html?dir=game%20objects/particle%20emitter/&q=
+citing for particles
+
+*/
 let config={
     type: Phaser.CANVAS,
     width: 640,
